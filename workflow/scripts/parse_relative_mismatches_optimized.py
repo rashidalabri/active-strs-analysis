17eab116372da20183abb6cdeb9a683a28f33b0c
rashidalabri <itsrashidalabri@gmail.com>
2021-08-25 06:29:19 -0700

    Update

--- workflow/scripts/parse_relative_mismatches_optimized.py
+++ workflow/scripts/parse_relative_mismatches_optimized.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 import pysam
 from collections import defaultdict
+from time import time
 
 extract_nodes_p = re.compile('(\d+)\[((?:\d+[A-Z])+)\]', re.IGNORECASE)
 extract_operations_p = re.compile('(\d+)([A-Z])', re.IGNORECASE)
@@ -86,11 +87,13 @@ print('Finished fetching {} reads. Parsing...'.format(sample))
 # Parse and store, while continously freeing up memory
 all_locus_ids = list(loci_cigars.keys())
 for locus_id in all_locus_ids:
+    t0 = time()
     locus_flank_data = LocusFlankData()
     for cigar_string in loci_cigars[locus_id]:
         locus_flank_data.parse_graph_cigar(cigar_string)
     df = locus_flank_data.to_df()
     df['locus_id'] = locus_id
     df = df.set_index(['locus_id', 'position'])
-    df.to_hdf(out_file_path, sample, format='table', append=True)
+    df.to_hdf(out_file_path, sample, format='table', append=True, min_itemsize={ 'locus_id' : 32 })
     del loci_cigars[locus_id]
+    print('Took {}s to proccess one locus.'.format(time()-t0))
